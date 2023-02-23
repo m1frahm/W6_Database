e746d9d19ea7f924154cc76aa2be47ccce2c213a
Mifrah Malik <mifrah.malik@gmail.com>
2023-02-23 12:20:31 -0500

    manually added a basic get request

--- index.js
+++ index.js
@@ -3,9 +3,15 @@ const cors = require("cors");
 
 const app = express();
 
+app.get("/api/books", (req, res) => {
+  let books = {
+    title: "A Court of Thorns and Roses",
+    author: "Sarah J. Mass",
+    format: "Kindle",
+  };
+});
+
 app.use(cors());
 
 const PORT = 4000;
 app.listen(PORT, () => console.log(`Server for W6 Project ${PORT}`));
-
-
