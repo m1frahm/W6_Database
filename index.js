0088474aaef8d3807a65220d1f4a6b698c4f9199
Mifrah Malik <mifrah.malik@gmail.com>
2023-02-23 12:17:29 -0500

    added cors function for security reasons to index.js

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-const PORT = 4000;
 
+app.use(cors());
+
+const PORT = 4000;
 app.listen(PORT, () => console.log(`Server for W6 Project ${PORT}`));
+
+
