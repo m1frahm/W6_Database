d901a8127cc53d859679683f252867b84f3fb1a7
Mifrah Malik <mifrah.malik@gmail.com>
2023-02-24 13:39:50 -0500

    commented out, testing something

--- index.js
+++ index.js
@@ -1,79 +1,78 @@
 const express = require("express");
 const cors = require("cors");
-//const books = require("./books.js");
+//const books = require("./books.js"); uncomment this
 const app = express();
 
-const books = [
-    {
-      isbn: "0553120441",
-      title: "Harry Potter and the Sorcerer's Stone",
-      author: "J.K. Rowling",
-      format: "Paperback",
-    },
-    {
-      isbn: "0679889175",
-      title: "Harry Potter and the Chamber of Secrets",
-      author: "J.K. Rowling",
-      format: "Paperback",
-    },
-    {
-      isbn: "9780857660282",
-      title: "Harry Potter and the Prisoner of Azkaban",
-      author: "J.K. Rowling",
-      format: "Paperback",
-    },
-    {
-      isbn: "0553096095",
-      title: "Harry Potter and the Goblet of Fire",
-      author: "J.K. Rowling",
-      format: "Hardcover",
-    },
-    {
-      isbn: "05530962356",
-      title: "Harry Potter and the Order of the Phoenix",
-      author: "J.K. Rowling",
-      format: "Hardcover",
-    },
-    {
-      isbn: "0553098976",
-      title: "Harry Potter and the Half-Blood Prince",
-      author: "J.K. Rowling",
-      format: "Hardcover",
-    },
-    {
-      isbn: "0553093476",
-      title: "Harry Potter and the Deathly Hallows",
-      author: "J.K. Rowling",
-      format: "Hardcover",
-    },
-    {
-      isbn: "0553115634",
-      title: "A Court of Thorns and Roses",
-      author: "Sarah J. Maas",
-      format: "Hardcover",
-    },
-    {
-      isbn: "0553115765",
-      title: "A Court of Mist and Fury",
-      author: "Sarah J. Maas",
-      format: "Hardcover",
-    },
-    {
-      isbn: "05531324567",
-      title: "A Court of Wings and Ruin",
-      author: "Sarah J. Maas",
-      format: "Hardcover",
-    },
-    {
-      isbn: "05531324511",
-      title: "A Court of Silver Flames",
-      author: "Sarah J. Maas",
-      format: "Hardcover",
-    },
-  ];
-  
-  //module.exports = BOOKS;
-  
+// const books = [
+//     {
+//       isbn: "0553120441",
+//       title: "Harry Potter and the Sorcerer's Stone",
+//       author: "J.K. Rowling",
+//       format: "Paperback",
+//     },
+//     {
+//       isbn: "0679889175",
+//       title: "Harry Potter and the Chamber of Secrets",
+//       author: "J.K. Rowling",
+//       format: "Paperback",
+//     },
+//     {
+//       isbn: "9780857660282",
+//       title: "Harry Potter and the Prisoner of Azkaban",
+//       author: "J.K. Rowling",
+//       format: "Paperback",
+//     },
+//     {
+//       isbn: "0553096095",
+//       title: "Harry Potter and the Goblet of Fire",
+//       author: "J.K. Rowling",
+//       format: "Hardcover",
+//     },
+//     {
+//       isbn: "05530962356",
+//       title: "Harry Potter and the Order of the Phoenix",
+//       author: "J.K. Rowling",
+//       format: "Hardcover",
+//     },
+//     {
+//       isbn: "0553098976",
+//       title: "Harry Potter and the Half-Blood Prince",
+//       author: "J.K. Rowling",
+//       format: "Hardcover",
+//     },
+//     {
+//       isbn: "0553093476",
+//       title: "Harry Potter and the Deathly Hallows",
+//       author: "J.K. Rowling",
+//       format: "Hardcover",
+//     },
+//     {
+//       isbn: "0553115634",
+//       title: "A Court of Thorns and Roses",
+//       author: "Sarah J. Maas",
+//       format: "Hardcover",
+//     },
+//     {
+//       isbn: "0553115765",
+//       title: "A Court of Mist and Fury",
+//       author: "Sarah J. Maas",
+//       format: "Hardcover",
+//     },
+//     {
+//       isbn: "05531324567",
+//       title: "A Court of Wings and Ruin",
+//       author: "Sarah J. Maas",
+//       format: "Hardcover",
+//     },
+//     {
+//       isbn: "05531324511",
+//       title: "A Court of Silver Flames",
+//       author: "Sarah J. Maas",
+//       format: "Hardcover",
+//     },
+//   ];
+
+//module.exports = BOOKS;
 
 // app.get("/api/books", (req, res) => {
 //   let books = {
@@ -108,10 +107,14 @@ app.get("/api/:booksID", cors(), async (req, res) => {
 
 app.use(cors());
 
-app.post("/api/books", cors(), async (req, res) => {
-    const book = req.body;
-    books
-}); 
+// app.post("/api/books", cors(), (req, res) => {
+
+//     const book = req.body; // body will contain what you do in your put request
+//     console.log(req);
+//     books.push(book);
+//    // res.send(`${book.title}`)
+//     console.log(book);
+// });
 
 //copied post request from postman
 // var myHeaders = new Headers();
