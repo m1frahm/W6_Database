754100a714c69487fdc8ced7cea4bbea5cfa181b
Mifrah Malik <mifrah.malik@gmail.com>
2023-02-24 15:50:38 -0500

    added comments for Chris

--- routes/books.js
+++ routes/books.js
@@ -13,8 +13,9 @@ const books = [
 
 //all routes in here are starting with /books
 router.get("/", (req, res) => {
-  console.log(books);
-  res.send("Hello");
+    //ATTENTION ON LINE 17
+  console.log(books); // this is where the error started, i wanted my browser to show the books array and it only showed up in my terminal
+     res.send("Hello");
 });
 
 export default router;
